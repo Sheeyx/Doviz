92af28fbf4b5799ad483d9dc3b56650db646ab31
Sheeyx <sheeyx00@gmail.com>
2023-10-03 16:41:46 +0900

    add changes

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿
+int operationNum = 0;
+ do
+{
+    Console.WriteLine();
+    Console.WriteLine("This program will convert your money as follow!:");
+    Console.WriteLine("1. Uzbek (so'm) => USD");
+    Console.WriteLine("2. USD => Uzbek (so'm)");
+    Console.WriteLine("3. Exit");
+    Console.Write("Enter a number: ");
+
+    try
+    {
+        operationNum = int.Parse(Console.ReadLine());
+        Console.Clear();
+    }
+    catch (FormatException formatException)
+    {
+        Console.WriteLine("The input is not in a correct format");
+    }
+    catch (OverflowException overflowException)
+    {
+        Console.WriteLine("Value is too large or too small, take a look data type.");
+    }
+    catch (Exception exception)
+    {
+        Console.WriteLine(exception.Message);
+    }
+
+    decimal exchangeRate;
+
+
+    switch (operationNum)
+    {
+        case 1:
+            Console.Write("Enter your exchange rate: ");
+            exchangeRate = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("Enter your budjet (Uzb so'm): ");
+            decimal  Amount = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine($"Your amount in usd is {Amount / exchangeRate}$ ");
+            break;
+
+        case 2:
+            Console.Write("Enter your exchange rate: ");
+            exchangeRate = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("Enter your budjet (Uzb so'm): ");
+            Amount = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine($"Your amount in uzb so'm is {Amount * exchangeRate} so'm");
+            break;
+
+        case 3:
+            Console.WriteLine("Thank you for using our application! ");
+            break;
+
+        default:
+            Console.WriteLine("Entered wrong operation, try again!!!");
+            break;
+    }
+} while (operationNum != 3);
--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # Doviz
 It helps convert money easily !
+
+# Currency Converter
+
+## Overview
+
+The Currency Converter is a console application that enables users to perform currency conversion between US Dollars (USD) and Uzbekistan Som (UZB). It allows users to select the conversion direction, enter the exchange rate, and input the amount they wish to convert.
+
+## Features
+
+- **Currency Conversion**: Convert between USD and UZB or vice versa.
+- **Exchange Rate Input**: Input custom exchange rates.
+- **Amount Conversion**: Calculate the equivalent amount based on the exchange rate.
+- **User-Friendly Interface**: Guided prompts for easy usage.
+
+## How to Use
+
+1. **Clone the Project**: Clone this project repository to your local machine:
+
+   ```shell
+   git clone https://github.com/yourusername/currency-converter.git
+
+Welcome to the Currency Converter!
+Select currency conversion direction:
+1. Uzbek (so'm) => USD
+2. USD => Uzbek (so'm)
+3. Exit
+
+Enter your choice: 2
+
+Enter the exchange rate : 10000
+
+Enter the amount to convert: 1000000
+
+1,000,000 UZB is equivalent to 100 USD
